26593dc82617474ed07aa6cd319620d676948f22
rboudhar001 <rboudhar001@gmail.com>
2020-02-25 00:55:29 +0000

    [0.0.3] Fix Test

--- modules/person/person-service/src/test/java/com/rboudhar001/service/impl/test/PersonLocalServiceImplTest.java
+++ modules/person/person-service/src/test/java/com/rboudhar001/service/impl/test/PersonLocalServiceImplTest.java
@@ -2,31 +2,22 @@ package com.rboudhar001.service.impl.test;
 
 import com.liferay.counter.kernel.service.CounterLocalServiceUtil;
 import com.rboudhar001.model.Person;
-import com.rboudhar001.service.PersonLocalService;
+import com.rboudhar001.service.PersonLocalServiceUtil;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.osgi.service.component.annotations.Reference;
 
 public class PersonLocalServiceImplTest {
-
-	// Service
-	private PersonLocalService mPersonLocalService;
-
-	@Reference(unbind = "-")
-	protected void setPersonService(PersonLocalService personLocalService) {
-		mPersonLocalService = personLocalService;
-	}
-
+	
 	@Test
 	public void addPersonTest() {
 
 		// Person
 		long personId = CounterLocalServiceUtil.increment(Person.class.getName());
-		Person person = mPersonLocalService.createPerson(personId);
+		Person person = PersonLocalServiceUtil.createPerson(personId);
 
 		// Service : Add Person
-		Person aux = mPersonLocalService.addPerson(person);
+		Person aux = PersonLocalServiceUtil.addPerson(person);
 
 		// Assert
 		Assert.assertNotNull(aux);
